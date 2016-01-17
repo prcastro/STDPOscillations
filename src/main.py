cfc79173be96670ac774e1bb9c5b3bc47a9a94ff
EstevaoVieira <estevovsky@gmail.com>
2016-01-17 14:42:42 -0200

    Partial version with oscillations

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ import numpy as np
 # 9) Parameter search
 # 10) Introduce more patterns
 
-def masquelier( simTime =    0.5 * second,    N       = 2000 ,    psp     =    0.8 * mV ,    tau     =   20   * msecond ,    V0      =  -55   * mV  ,    Vt      =  -50   * mV  ,    Vr      =  -60   * mV  ,    El      =  -51   * mV ):
+def masquelier( simTime = 0.5*second,   N = 2000 ,    psp = 3*mV ,    tau  = 20*msecond ,    V0 = -55*mV  ,    Vt  = -54*mV  ,    Vr = -65*mV  ,    El  = -60*mV, oscilFreq = 0, oscilAmp = 1):
     '''This file executes the simulations (given the parameters),
     of Masquelier's model for learning and saves the results in
     appropriate files.'''
@@ -27,17 +27,23 @@ def masquelier( simTime =    0.5 * second,    N       = 2000 ,    psp     =    0
 #    Vr      =  -60   * mV      # reset value
 #    El      =  -51   * mV      # resting potential (same as the reset)
     # Neural Model
+    oscilString = "I ="+ str(oscilAmp) +"*sin(pi/2 +"+str(oscilFreq)+"*t) : volt"
+
     neuronEquations =  Equations('''
-    dV/dt = -(V-El)/tau : volt
-    ''')
+    dV/dt = I -(V-El)/tau : volt
+    '''+ oscilString)
+
     # Create neuron groups and set initial conditions
     inputSilent = NeuronGroup(N=N, model=neuronEquations, threshold=Vt, reset=Vr)
-    inputSilent.V = V0*ones(N)
+    inputSilent.V = Vr + rand(N)*(Vt - Vr)#V0*ones(N)
     # Stablish drive
-    poisson = PoissonGroup(N,rates=20*Hz)
+    poisson = PoissonGroup(N,rates=15*Hz)
+    #oscil_drive =
+
     # Connect groups
     inputDrive = Connection(poisson, inputSilent)
     inputDrive.connect_one_to_one(poisson, inputSilent, weight=psp)
+
     # Mesurement devices
     spikes = SpikeMonitor(inputSilent)
     voltimeter = StateMonitor(inputSilent, 'V', record=0)
