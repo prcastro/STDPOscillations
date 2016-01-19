6b14f57f74278b9833c9d55b65db402b04085b01
EstevaoVieira <estevovsky@gmail.com>
2016-01-19 11:11:58 -0200

    Pattern changed to last 1800:2000, and gray on botton when pattern is present

--- src/newpatt.py
+++ src/newpatt.py
@@ -11,18 +11,21 @@ def patterns(N, totalTime):
 
     times.append(totalTime) #*ms
     ptimes = []
-    t=0
+    t=exponential(1250)
     while t<totalTime:
-        t += exponential(1250)
         ptimes.append(t) #*ms
+        t += exponential(1250)
     times+=ptimes
     times=sort(times)
-    activations = rand(len(times), N)
+    activations = rand(len(times), N+21)
 
 
     for (i,t) in enumerate(times):
         if t in ptimes: #t*second
-            activations[i, 0:200] = pattern
+            activations[i, 1800:2000] = pattern
+            activations[i,N:N+20] = .8
+        else:
+            activations[i,N:N+20] = 1
     plotpats(times, activations)
     return TimedArray(activations,times)
 
